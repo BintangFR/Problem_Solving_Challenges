d8b9a94385a7bffb290f048f0636c51ae2e03941
SovietChan <52289632+SovietChan@users.noreply.github.com>
2021-11-09 15:32:22 +0700

    Problem Solved

--- Assets/Scripts/BallController/BallController.cs
+++ Assets/Scripts/BallController/BallController.cs
@@ -13,23 +13,28 @@ public class BallController : MonoBehaviour
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
-        SetVelocity(initialSpeed);
     }
     
     void FixedUpdate()
     {
-        inputVector.x = Input.GetAxis("Horizontal");
-        inputVector.y = Input.GetAxis("Vertical");
-        MoveCircle(inputVector, initialSpeed);
-    }
-
-    private void SetVelocity(float velocity)
-    {
-        rigidbody2D.velocity = Random.onUnitSphere * velocity;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            inputVector = GetCursorDirection();
+            MoveCircle(inputVector, initialSpeed);
+        }
     }
 
     private void MoveCircle(Vector2 input, float velocity)
     {
         rigidbody2D.velocity = inputVector * velocity;
+    }
+
+    public Vector2 GetCursorDirection()
+    {
+        //Debug.Log("Clicked");
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, this.transform.position.z));
+        Vector3 direction = worldMousePosition - this.transform.position;
+        direction.Normalize();
+        return (Vector2)direction;
     }
 }
