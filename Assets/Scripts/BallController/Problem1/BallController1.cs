a98e4884ee7a9a1521adc195b87622f3dbecb3dc
SovietChan <52289632+SovietChan@users.noreply.github.com>
2021-11-11 21:49:17 +0700

    Fixing Few Bug

---
+++ Assets/Scripts/BallController/Problem1/BallController1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController1 : MonoBehaviour
+{
+    [SerializeField]
+    public float initialSpeed = 30f;
+
+    private Rigidbody2D rigidbody2D;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        SetVelocity(initialSpeed);
+    }
+
+    private void SetVelocity(float velocity)
+    {
+        rigidbody2D.velocity = Random.onUnitSphere * velocity;
+    }
+}
---
+++ Assets/Scripts/BallController/Problem4/BallController4.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController4 : MonoBehaviour
+{
+    [SerializeField]
+    public float initialSpeed = 30f;
+
+    private Vector2 inputVector = Vector2.zero;
+    private Rigidbody2D rigidbody2D;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        SetVelocity(initialSpeed);
+    }
+    
+    void FixedUpdate()
+    {
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.y = Input.GetAxis("Vertical");
+        MoveCircle(inputVector, initialSpeed);
+    }
+
+    private void SetVelocity(float velocity)
+    {
+        rigidbody2D.velocity = Random.onUnitSphere * velocity;
+    }
+
+    private void MoveCircle(Vector2 input, float velocity)
+    {
+        rigidbody2D.velocity = inputVector * velocity;
+    }
+}
