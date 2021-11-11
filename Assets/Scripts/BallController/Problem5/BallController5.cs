576bb1e00d766984308e39376385dd56fff46176
SovietChan <52289632+SovietChan@users.noreply.github.com>
2021-11-11 22:11:24 +0700

    Few Bug Fix

---
+++ Assets/Scripts/BallController/Problem5/BallController5.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController5 : MonoBehaviour
+{
+    [SerializeField]
+    public float initialSpeed = 30f;
+
+    private Vector2 inputVector = Vector2.zero;
+    private Rigidbody2D rigidbody2D;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            inputVector = GetCursorDirection();
+            MoveCircle(inputVector, initialSpeed);
+        }
+    }
+
+    private void MoveCircle(Vector2 input, float velocity)
+    {
+        rigidbody2D.velocity = inputVector * velocity;
+    }
+
+    public Vector2 GetCursorDirection()
+    {
+        //Debug.Log("Clicked");
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, this.transform.position.z));
+        Vector3 direction = worldMousePosition - this.transform.position;
+        direction.Normalize();
+        return (Vector2)direction;
+    }
+}
